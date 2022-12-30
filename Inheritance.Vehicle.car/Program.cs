f2e5a228661f4be756d6ae13e7d86ee3d94d0431
kimi <kimi@DESKTOP-HLSO57E>
2022-12-30 18:51:15 +0330

    Add project files.

---
+++ Inheritance.Vehicle.car/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Inheritance.Vehicle.car
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car();
+
+            Console.WriteLine(car.speed);
+            Console.WriteLine(car.wheels);
+            car.go();
+
+            Console.ReadKey();
+        }
+    }
+        class Vehicle
+        {
+
+            public int speed = 0;
+
+            public void go()
+            {
+                Console.WriteLine("This vehice is moving...");
+            }
+        }
+    class Car : Vehicle
+    {
+        public int wheels = 4;
+    }
+}
